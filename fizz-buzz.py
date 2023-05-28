0692c1a9835dbe5af4ffa44513c6bbb5637affe3
Artur Kwiatkowski <53915576+kffiatek@users.noreply.github.com>
2023-05-28 19:40:00 +0200

    hyphon and game size added

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,12 +1,15 @@
 print("Fizz-Buzz by kffiatek")
 
+game_size = 100
 factors_of_interest = {3 : "Fizz", 5 : "Buzz"}
 
-for i in range(1,101):
+for i in range(1,game_size+1):
   result=""
   player=i % 2
   for factor in factors_of_interest:
-    if (i % factor == 0): result=result+factors_of_interest[factor]
+    if (i % factor == 0): 
+      if result: result+="-"
+      result+=factors_of_interest[factor]
   if not result: result = str(i)
   if not player: result = 10*(" ") + result
   print(result)
