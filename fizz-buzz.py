194b1c7582b3f8a5bcf47a28c4ea2cf15d3b0aa4
Artur Kwiatkowski <53915576+kffiatek@users.noreply.github.com>
2023-05-28 14:06:30 +0200

    FizzBuzz finished

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -4,7 +4,9 @@ factors_of_interest = {3 : "Fizz", 5 : "Buzz"}
 
 for i in range(1,101):
   result=""
+  player=i % 2
   for factor in factors_of_interest:
     if (i % factor == 0): result=result+factors_of_interest[factor]
-  if not result: result = i
+  if not result: result = str(i)
+  if not player: result = 10*(" ") + result
   print(result)
